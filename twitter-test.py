6ba53cc40701d7a6e24f64eaf67e52081b3fa7d8
Marcelo Destefani <marcelo@hastapronto.cl>
2020-08-03 21:41:31 +0200

    api test

--- twitter-test.py
+++ twitter-test.py
@@ -9,3 +9,8 @@ auth = tweepy.OAuthHandler(credentials['consumer_key'], credentials['consumer_se
 auth.set_access_token(credentials['access_token'], credentials['access_secret'])
 
 api = tweepy.API(auth)
+
+#Make call on home timeline, print each tweets text
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
