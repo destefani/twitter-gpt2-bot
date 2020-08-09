0cb6be11b9ce04ebe596ff458814e93251181b42
Marcelo Destefani <marcelo.destefani@zalando.de>
2020-08-09 21:56:45 +0200

    credentials fix

--- twitter-test.py
+++ twitter-test.py
@@ -6,7 +6,7 @@ with open('credentials/credentials.json') as json_file:
 
 
 auth = tweepy.OAuthHandler(credentials['consumer_key'], credentials['consumer_secret'])
-auth.set_access_token(credentials['access_token'], credentials['access_secret'])
+auth.set_access_token(credentials['access_token'], credentials['access_token_secret'])
 
 api = tweepy.API(auth)
 
